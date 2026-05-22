cc083a0acdb9b00a2f8a9d346bb9a2e76a1914e2
bartoshqr <rtek.rko@gmail.com>
2026-05-22 09:38:49 +0200

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: "passthrough",
+  }),
   env: {
     schema: {
       SUPABASE_URL: envField.string({ context: "server", access: "secret", optional: true }),
